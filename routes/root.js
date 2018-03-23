fdee9f6ff68f362789e9a8420556851ead7c2be9
Juan Carlos Gonzalez <jcgonzalezmolina@gmail.com>
2018-03-22 23:21:21 -0400

    updates

--- routes/root.js
+++ routes/root.js
@@ -1,12 +1,16 @@
 'use strict';
 
+///////////////////////////
+/////   root route   /////
+/////////////////////////
+
 const bodyParser = require('body-parser');
 
 const root = (router) => {
   router.use(bodyParser.json());
   router.use((req, res, next) => {
     res.send('up and running!');
-    // next();
+    next();
   });
 }
 
