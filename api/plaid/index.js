ac0b84ac9967b9fc21d566e5283a472798d553f3
Juan Carlos Gonzalez <jcgonzalezmolina@gmail.com>
2018-03-22 23:25:20 -0400

    started laying out back end environment for plaid functions

---
+++ api/plaid/index.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const moment = require('moment');
+const plaid = require('plaid');
+
+let {
+  client_id,
+  secret,
+  public_key,
+  env,
+  // user specific
+  access_token,
+  public_token,
+  item_id
+} = require('../../config/index').init().plaid;
+
+const client = new plaid.Client(
+  client_id,
+  secret,
+  public_key,
+  plaid.environments[env]
+);
+
+console.log(client);
