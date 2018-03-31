ce3e12358dcaa98c1842fffb1f776a96be64eb4b
jc <javascriptdeveloper17@gmail.com>
2018-03-31 16:11:47 -0400

    mockup registration system in place

--- routes/user/register.js
+++ routes/user/register.js
@@ -2,17 +2,52 @@
 /////   registration POST route   /////
 //////////////////////////////////////
 
+const User = require('../../db/schemas/User');
+
 const register = (router) => {
   router.post('/', (req, res, next) => {
     let newUser = req.body;
     console.log(newUser);
     // if conditions pass, redirect to dashboard
     // otherwise, redirect to join with alerts
-    res.json({
-      msg: 'user registration capabilites under construction'
-    });
-    next();
+    // res.redirect('/join');
+    User.create(newUser)
+      .then(user => {
+        let msg = 'User created';
+        console.log(msg + '\n' + user);
+        res.json({ msg });
+        next();
+      })
+      .catch(err => {
+        let msg = 'Error creating user';
+        console.log(msg + '\n' + err);
+        res.json({ msg });
+        next();
+      });
   });
 };
 
 module.exports = register;
+
+
+
+// let newUser = new User(req.body);
+// User.register(newUser, function(err, user) {
+//   if (err) {
+//     let msg = 'Error creating user';
+//     console.log(msg + '\n' + err);
+//     res.json({ msg });
+//     next();
+//   }
+
+//   let msg = 'User created';
+//   console.log(msg + '\n' + user);
+//   res.json({ msg });
+//   next();
+
+// });
+
+// res.json({
+//   msg: 'user registration capabilites under construction'
+// });
+// next();
