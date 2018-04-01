4a18436851f8ec321858d60188a5ae598431b8ac
jc <javascriptdeveloper17@gmail.com>
2018-03-31 23:18:52 -0400

    left some notes for next sitting

--- routes/user/login.js
+++ routes/user/login.js
@@ -3,11 +3,21 @@
 ///////////////////////////////
 
 const login = (router) => {
-  router.post('/', (req, res, next) => {
+  router.post('/', /* authenticationFunction, */ (req, res, next) => {
     let returningUser = req.body;
     console.log(returningUser);
     // if auth successful, redirect to dashboard.
     // otherwise, redirect to login with alert
+
+    // 1. Use value of username field to search
+    //    for user by email or number - $or: { phone, email }
+    // 2. If User not found, notify and redirect to login (send back msg)
+    // 3. If User found and authentication successful, redirect to dashboard
+
+    // 3.1. IF User is found by number or email, retrieve _id
+    // 3.2. With _id at hand, look in Credentials collection by userId
+    // 3.2  Compare hashes (password). MAKE SURE TO HASH PASSWORD ON FRONT END;
+    //      DO NOT SEND OVER PLAIN PASSWORD.
     res.json({
       msg: 'user authentication capabilites under construction'
     });
